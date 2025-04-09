d5e427096d2747e7674d2a9fc02dce5830e0bce3
WillMoss11 <william.moss@keyin.com>
2025-04-08 22:45:52 -0230

    insert into BST

--- src/main/java/DSA_Final/BinarySearchTree.java
+++ src/main/java/DSA_Final/BinarySearchTree.java
@@ -20,5 +20,28 @@ public class BinarySearchTree {
         this.root = null;
     }
 
+    // To insert a new value into the BST
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    // To insert a value in the tree
+    private Node insertRec(Node root, int data) {
+        // If the tree is empty, return a new node
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        // go down the tree
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
 
 }
