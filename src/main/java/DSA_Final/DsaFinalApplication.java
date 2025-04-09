595dada4433751a010e15c3681462c078d823f64
WillMoss11 <william.moss@keyin.com>
2025-04-08 22:25:15 -0230

    Springboot

--- src/main/java/DSA_Final/DsaFinalApplication.java
+++ src/main/java/DSA_Final/DsaFinalApplication.java
@@ -3,5 +3,9 @@ package DSA_Final;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DsaFinalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DsaFinalApplication.class, args);
+    }
 }
