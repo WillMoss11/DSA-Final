f8a1d7b89d732b655b8eb7f8ac7dc7bd320ff48e
WillMoss11 <william.moss@keyin.com>
2025-04-08 22:29:35 -0230

    enter-numbers added/imported

--- src/main/java/DSA_Final/UserInputController.java
+++ src/main/java/DSA_Final/UserInputController.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserInputController {
+
+    @GetMapping("/enter-numbers")
+    public String showEnterNumbersPage(Model model) {
+        model.addAttribute("message", "Enter your numbers to create a binary search tree");
+        return "enter-numbers"; // This comes from the resources/templates
+    }
 }
