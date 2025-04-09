93a970edc074c19faff7ad97b74c47b329a02fb1
WillMoss11 <william.moss@keyin.com>
2025-04-08 22:40:56 -0230

    Node class data

--- src/main/java/DSA_Final/BinarySearchTree.java
+++ src/main/java/DSA_Final/BinarySearchTree.java
@@ -1,4 +1,18 @@
 package DSA_Final;
 
 public class BinarySearchTree {
+
+    // Node class for each element in the tree
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
 }
