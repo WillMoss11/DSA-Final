40b5874391857cc8c121eb844f03509542be05b7
WillMoss11 <william.moss@keyin.com>
2025-04-08 22:57:05 -0230

    Process numbers

--- src/main/java/DSA_Final/UserInputController.java
+++ src/main/java/DSA_Final/UserInputController.java
@@ -9,9 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserInputController {
 
+    private final BinarySearchTree bst;
+
+    // initialize the BST
+    public UserInputController {
+        this.bst = new BinarySearchTree();
+    }
+
     @GetMapping("/enter-numbers")
     public String showEnterNumbersPage(Model model) {
         model.addAttribute("message", "Enter your numbers to create a binary search tree");
         return "enter-numbers"; // This comes from the resources/templates
+    }
+
+    // handle for processing numbers
+    @PostMapping("/process-numbers")
+    public String processNumbers(@RequestParam String number, Model model) {
+        // split the numbers input by a comma, convert to integers n insert into the BST
+        String[] numberStrings = number.split(",");
+        for (String numStr : numberStrings) {
+            int num = Integer.parseInt(numStr.trim());
+            bst.insert(num);
+        }
     }
 }
