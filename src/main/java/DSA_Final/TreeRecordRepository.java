a0dbe415fb01d7e2d52427f952a1e5bb36b01f1d
WillMoss11 <william.moss@keyin.com>
2025-04-09 23:46:06 -0230

    Updated

--- src/main/java/DSA_Final/TreeRecordRepository.java
+++ src/main/java/DSA_Final/TreeRecordRepository.java
@@ -2,5 +2,6 @@ package DSA_Final;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class TreeRecordRepository {
+public interface TreeRecordRepository extends JpaRepository<TreeRecord, Long> {
+    // don’t need to write anything here I don't' think
 }
