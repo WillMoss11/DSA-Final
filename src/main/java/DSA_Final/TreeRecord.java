a9e803615a44e7d6d50b6d028a889997c4f6bbd0
WillMoss11 <william.moss@keyin.com>
2025-04-09 23:43:36 -0230

    bst n numbers added

--- src/main/java/DSA_Final/TreeRecord.java
+++ src/main/java/DSA_Final/TreeRecord.java
@@ -2,5 +2,17 @@ package DSA_Final;
 
 import jakarta.persistence.*;
 
+@Entity
 public class TreeRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String inputNumbers;
+
+    private String bstResult;
+
+    // default
+
 }
